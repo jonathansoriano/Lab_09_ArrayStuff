60c4f16cde7367e21aa8a9fa978cad5017999a7f
jonathansoriano <158002831+jonathansoriano@users.noreply.github.com>
2024-03-04 22:23:43 -0500

    Not finished, but almost

---
+++ src/Main.java
@@ -0,0 +1,66 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Random rnd = new Random();
+        Scanner in = new Scanner(System.in);
+        int SIZE = 100;
+        int [] dataPoint = new int[SIZE];
+        int sum = 0;
+        int ave = 0;
+        int num = 0;
+        int min = 0;
+        int max = 0;
+        boolean foundNum = false;
+
+        for (int i = 0; i < dataPoint.length; i++){
+            dataPoint[i] = rnd.nextInt(100) + 1; // range of 1-100
+        }
+        for (int i = 0; i < dataPoint.length; i++){
+            System.out.print(dataPoint[i] + " | ");
+        }
+        for (int i = 0; i < dataPoint.length; i++){
+            sum += dataPoint[i];
+            System.out.println("\nThe sum of this array is " + sum);
+        }
+        ave = sum/ dataPoint.length;
+        System.out.println("\nThe average of this array is " + ave);
+
+        num = SafeInput.getRangedInt(in, "Enter an integer ", 1, 100 );
+
+        for (int i = 0; i < dataPoint.length; i++){
+            if (dataPoint[i] == num){
+                foundNum = true;
+                System.out.println("Your number " + num + " was found in location " + i);
+            }
+        }
+        if (!foundNum){
+            System.out.println("Your number " + num + " was not found.");
+        }
+        for (int i = 0; i < dataPoint.length; i++){
+            if (dataPoint[i] == num){
+                foundNum = true;
+                System.out.println("\n\nYour number " + num + " was found in location " + i);
+                break;
+            }
+        }
+        if (!foundNum){
+            System.out.println("Your number " + num + " was not found.");
+        }
+
+        for (int i = 0; i < dataPoint.length; i++){
+            if (min > dataPoint[i]){
+                min = dataPoint[i];
+            }
+        }
+        System.out.println("\nThe minimum value for this array is " + min);
+        for (int i = 0; i < dataPoint.length; i++){
+            if (max < dataPoint[i]){
+                max = dataPoint[i];
+            }
+        }
+        System.out.println("\nThe maximum value for this array is " + max);
+    }
+}
+
